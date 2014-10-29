fedf40072d41d0a60569ddf855b52aba09db1068
prakashash18 <prakashash18@gmail.com>
2014-10-29 21:26:56 +0800

    startup

--- StartUp.py
+++ StartUp.py
@@ -1,17 +1,17 @@
 import subprocess
 
 # #startup dispatcher
- #output = open('dispatcher.txt', 'w')
- dispatcher = subprocess.Popen(['easyNav-pi-dispatcher', '>', 'dispatcher.txt', '2>&1'], shell=True)
-    #dispatcher.communicate()[0]
+#output = open('dispatcher.txt', 'w'
+dispatcher = subprocess.Popen('easyNav-pi-dispatcher > dispatcher.txt 2>&1', shell=True)
+#dispatcher.communicate()[0]
 
 # #startup Nav
-nav = subprocess.Popen(['easyNav-pi-nav', '>', 'dispatcher.txt', '2>&1'], shell=True)
+nav = subprocess.Popen('easyNav-pi-nav > dispatcher.txt 2>&1', shell=True)
 
 
 #start voice
 #output = open('voice.txt', 'w')
-voice = subprocess.Popen(['sudo', 'python' ,'/home/pi/repos/easyNav-IO/voice.py', '>','voice.txt', '2>&1'], shell=True)
+voice = subprocess.Popen('sudo python voice.py > voice.txt 2>&1', shell=True)
 
 #voice.communicate()
 

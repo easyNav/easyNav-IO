f0ec9663a7d712ad0b27123966d38f6f6302acbb
prakashash18 <prakashash18@gmail.com>
2014-10-29 21:06:08 +0800

    startup

--- StartUp.py
+++ StartUp.py
@@ -1,19 +1,19 @@
 import subprocess
 
 # #startup dispatcher
-with open('dispatcher.txt', 'w+') as output:
-    dispatcher = subprocess.Popen('easyNav-pi-dispatcher', stdout=output, universal_newlines=True)
-    dispatcher.communicate()[0]
+ #output = open('dispatcher.txt', 'w')
+ dispatcher = subprocess.Popen(['easyNav-pi-dispatcher', '>', 'dispatcher.txt', '2>&1'], shell=True)
+    #dispatcher.communicate()[0]
 
 # #startup Nav
-# with open('navigation.txt', 'w') as output:
-#     nav = subprocess.Popen('easyNav-pi-nav', stdout=output)
-#     #nav.communicate()
+nav = subprocess.Popen(['easyNav-pi-nav', '>', 'dispatcher.txt', '2>&1'], shell=True)
+
 
 #start voice
-voice = subprocess.Popen(['sudo', 'python' ,'/home/pi/repos/easyNav-IO/voice.py'])
-print voice
-    #voice.communicate()
+#output = open('voice.txt', 'w')
+voice = subprocess.Popen(['sudo', 'python' ,'/home/pi/repos/easyNav-IO/voice.py', '>','voice.txt', '2>&1'], shell=True)
+
+#voice.communicate()
 
 #startup Voice
 #os.system("sudo python /easyNav-IO/voice.py")

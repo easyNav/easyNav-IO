92d18ec5bd27632c2030ef0b76c700e54279c41c
prakashash18 <prakashash18@gmail.com>
2014-11-10 13:36:33 +0800

    -m "a"

--- voice.py
+++ voice.py
@@ -7,6 +7,7 @@ import KeypadLogic
 import multiprocessing
 import time
 import GetLocations
+
 import os
 
 
@@ -20,19 +21,19 @@ class Notifications(object):
 		# self.DISPATCHER_PORT = 9002
 		# self.dispatcherClient = DispatcherClient(port=self.DISPATCHER_PORT)
 
-        ## Attach event listeners
-		self.attachEvents(self.speaker)
+		## For collision detection: lock navigation until ready
+        self.collisionLocked = False
+        self.obstacle = None
 
-	def start(self):
-		self.dispatcherClient.start()
-		
-		#run notifier forever
-		while(1):
-			time.sleep(0.1)
+
+        self.infotosay =None
+
+         ## Attach event listeners
+		self.attachEvents(self.speaker)
 
 
 	def attachEvents(self, mic):
-        ## clear all signals
+		## clear all signals
 		smokesignal.clear()
 		text = ""
 		@smokesignal.on('say')
@@ -40,16 +41,66 @@ class Notifications(object):
 			print "Info from Nav"
 			infoFromNav = eval(args.get('payload'))
 			print infoFromNav
-			infotosay = infoFromNav["text"]
-			print infotosay
-			print "Info from Nav before Mic"
-			mic.say(infotosay)
+			self.infotosay = infoFromNav["text"]
+			print self.infotosay
+
+			# print "Info from Nav before Mic"
+			# mic.say(infotosay)
+
+		@smokesignal.on('obstacle')
+		def onObstacle(args):
+			response = int(json.loads(args.get('payload')).get('status'))
+			if (response == 0):
+				self.obstacle = None
+				return # Do not set collision locked
+			elif (response == 1):
+				self.obstacle = 'FRONT'
+			elif (response == 2):
+				self.obstacle = 'LEFT'
+			elif (response == 3):
+				self.obstacle = 'RIGHT'
+
+			# If collision, set to true
+			self.collisionLocked = True
+
+	def start(self):
+		self.dispatcherClient.start()
+
+		# self.collisionLocked = False
+		# self.obstacle = None
+		
+		#run notifier forever
+		while(1):
+
+			## Collision detection first
+			if (self.collisionLocked):
+				if (self.obstacle == None):
+					# Unlock collisionLocked
+					self.speaker.say('Obstacle cleared.  Move forward!')
+					self.collisionLocked = False
+					time.sleep(2)
+
+				elif (self.obstacle == 'FRONT'):
+					self.speaker.say('Obstacle ahead.  Turn 45 degrees left or right!')
+
+				elif (self.obstacle == 'LEFT'):
+					self.speaker.say('Obstacle on the left!')
+
+				elif (self.obstacle == 'RIGHT'):
+					self.speaker.say('Obstacle on the right!')
+
+				# Do not execute below
+			else: 
+				if(self.infotosay != None):
+					self.speaker.say(self.infotosay)
+					self.infotosay=None
+
+			time.sleep(0.1)
 
 	
-
 class Voice(object):
 
-	#HOST_ADDR = "http://localhost:1337/"
+	HOST_ADDR = "http://localhost:1337/"
 	def __init__(self): 
 
 		self.speaker = speaker.newSpeaker()
@@ -61,8 +112,8 @@ class Voice(object):
 		self.inputBuffer =[]
 
 		#update locations
-		self.getLocation = GetLocations.Locations()
-		self.getLocation.getLoc()
+		# self.getLocation = GetLocations.Locations()
+		# self.getLocation.getLoc()
 
 	def presentMenu(self):
 		self.speaker.say("Key * 1 # to choose map and level and locations")
@@ -115,43 +166,120 @@ class Voice(object):
 
 		return -1
 
+	def isCancel(self, inputVal):
+		if(inputVal == '999'):
+			self.speaker.say("Cancelling operation")
+			return True
+		return False
+
 	def start(self):
 		self.dispatcherClient.start()
 		#present menu
 		#self.presentMenu();
 
 		#keep listening for keypad activity
-		print "in start"
-
 		try:
 
-			print "try statrement"
 			while(1):
-				print "before getInput"
 				self.inputBuffer = KeypadLogic.getInput(self.speaker)
 
-				print "in infinite"
 
 				strInput = ''.join(self.inputBuffer)
 				print strInput
 
 				if (strInput == '*1'):
-					buildingBuf = []
-					levelBuff=[]
+
+					startBuildingBuf = []
+					startLevelBuff=[]
 					startNodeBuff=[]
-					endNodeBuff=[]	
 
-					self.speaker.say("Key in building ID")
-					buildingBuf = KeypadLogic.getInput(self.speaker)
+					endBuildingBuf = []
+					endLevelBuff=[]
+					endNodeBuff=[]
 
-					self.speaker.say("Key in level ID")
-					levelBuff = KeypadLogic.getInput(self.speaker)
 
-					self.speaker.say("Key in start point ID")
+					self.speaker.say("Key in start building ID, one for com one, two for com two")
+					startBuildingBuf = KeypadLogic.getInput(self.speaker)
+					strStartBuildingID = ''.join(startBuildingBuf)
+					
+					if(self.isCancel(strStartBuildingID)):
+						continue
+					else:
+						self.speaker.say("you have chosen building " + strStartBuildingID)
+
+
+					self.speaker.say("Key in start level ID, zero is basement.")
+					startLevelBuff = KeypadLogic.getInput(self.speaker)
+					strStartLevelID = ''.join(startLevelBuff)
+
+					if(self.isCancel(strStartLevelID)):
+						continue
+					else:
+						self.speaker.say("you have entered level " + strStartLevelID)
+
+
+					self.speaker.say("Key in start node ID")
 					startNodeBuff = KeypadLogic.getInput(self.speaker)
+					strStartNodeID = ''.join(startNodeBuff)
+					if(self.isCancel(strStartNodeID)):
+						continue
+					else:
+						self.speaker.say("you have entered start node " + strStartNodeID)
 
-					self.speaker.say("Key in destination ID")
+
+					self.speaker.say("Key in destination building ID ")
+					endBuildingBuf = KeypadLogic.getInput(self.speaker)
+					strEndBuildingID = ''.join(endBuildingBuf)
+					if(self.isCancel(strEndBuildingID)):
+						continue
+					else:
+						self.speaker.say("you have entered building " + strEndBuildingID)
+
+					self.speaker.say("Key in destination level ID")
+					endLevelBuff = KeypadLogic.getInput(self.speaker)
+					strEndLevelID = ''.join(endLevelBuff)
+					if(self.isCancel(strEndLevelID)):
+						continue
+					else:
+						self.speaker.say("you have entered level " + strEndLevelID)
+
+					self.speaker.say("Key in destination node ID")
 					endNodeBuff = KeypadLogic.getInput(self.speaker)
+					strEndNodeID = ''.join(endNodeBuff)
+					if(self.isCancel(strEndNodeID)):
+						continue
+					else:
+						self.speaker.say("you have entered destination node " + strEndNodeID)
+
+
+					#construct start point SUid
+					strStartSUID = strStartBuildingID+strStartLevelID+strStartNodeID
+					print strStartSUID
+					#construct dest SUid
+					strEndSUID = strEndBuildingID+strEndLevelID+strEndNodeID
+					print strEndSUID
+
+					try:
+
+						startSUID = int(strStartSUID)
+						endSUID = int(strEndSUID)
+
+						#get coord
+						r = requests.get(Locations.HOST_ADDR + "node?SUID=" +startSUID)
+						for location in r.json():
+							startCoord = location['loc']
+
+						self.dispatcherClient.send(9003, "starting", eval(startCoord))
+						self.dispatcherClient.send(9001, "newPath", {"from":startSUID, "to": endSUID})
+
+					except:
+						self.speaker.say("Invalid ID")
+
+
+					self.speaker.say("Routing!")
+
+
+					#find builiding map and get coordinates
 
 					del buildingBuf[:]
 					del levelBuff[:]
@@ -159,46 +287,45 @@ class Voice(object):
 					del endNodeBuff[:]
 
 				elif (strInput == '*2'):
-					startNodeBuff=[]
-					endNodeBuff=[]
+					# startNodeBuff=[]
+					# endNodeBuff=[]
 
+					# self.speaker.say("Key in start point ID")
+					# startNodeBuff = KeypadLogic.getInput(self.speaker)
+					# strStart = ''.join(startNodeBuff)
+					# self.speaker.say("you have entered " + strStart)
+					# print strStart
 
-					self.speaker.say("Key in start point ID")
-					startNodeBuff = KeypadLogic.getInput(self.speaker)
-					strStart = ''.join(startNodeBuff)
-					self.speaker.say("you have entered " + strStart)
-					print strStart
+					# self.speaker.say("Key in destination ID")
+					# endNodeBuff = KeypadLogic.getInput(self.speaker)
+					# strEnd = ''.join(endNodeBuff)
+					# self.speaker.say("you have entered " + strEnd)
 
-					self.speaker.say("Key in destination ID")
-					endNodeBuff = KeypadLogic.getInput(self.speaker)
-					strEnd = ''.join(endNodeBuff)
-					self.speaker.say("you have entered " + strEnd)
+					# #get start coord to send to Cruncher
+					# print "before getSUID"
+					# fileIndex = self.getSUIDIndex(int(strStart))
+					# startCoord = self.getCoord(fileIndex)
+					# print startCoord
 
-					#get start coord to send to Cruncher
-					print "before getSUID"
-					fileIndex = self.getSUIDIndex(int(strStart))
-					startCoord = self.getCoord(fileIndex)
-					print startCoord
+					# try:
 
-					try:
+					# 	self.dispatcherClient.send(9003, "starting", eval(startCoord))
+					# 	self.dispatcherClient.send(9001, "newPath", {"from":int(strStart), "to": int(strEnd)})
+					# except ValueError:
+					# 	self.speaker.say("Error, key in a proper ID")
 
-						self.dispatcherClient.send(9003, "starting", eval(startCoord))
-						self.dispatcherClient.send(9001, "newPath", {"from":int(strStart), "to": int(strEnd)})
-					except ValueError:
-						self.speaker.say("Error, key in a proper ID")
-
-					del startNodeBuff[:]
-					del endNodeBuff[:]
+					# del startNodeBuff[:]
+					# del endNodeBuff[:]
 
 				elif (strInput == '*3'):
-					self.speaker.say("Stopping previous navigation")
+					# self.speaker.say("Stopping previous navigation")
 
-					endNodeBuff=[]
+					# endNodeBuff=[]
 
-					self.speaker.say("Key in destination ID")
-					endNodeBuff = KeypadLogic.getInput(self.speaker)
+					# self.speaker.say("Key in destination ID")
+					# endNodeBuff = KeypadLogic.getInput(self.speaker)
 
-					del endNodeBuff[:]
+					# del endNodeBuff[:]
 
 				elif (strInput == '*4'):
 					self.speaker.say("Ending EasyNav, Are you sure?")
@@ -231,9 +358,11 @@ class Voice(object):
 				with open("myIp.txt") as IPText:
 					for line in IPText:
 						if "inet" in line:
-							self.mic.say(line)
-
-
+							line = line.replace("inet", "")
+							line = line.replace("addr:", "")
+							lineDic = line.split(" ")
+							self.mic.say(lineDic[0])
+							print lineDic[0]
 
 				time.sleep(0.1)
 		except:

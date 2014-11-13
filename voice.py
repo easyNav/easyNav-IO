2a4e718389992c48aa7508a6762178a7aae29d73
prakashash18 <prakashash18@gmail.com>
2014-11-13 20:12:29 +0800

    *2

--- voice.py
+++ voice.py
@@ -210,11 +210,9 @@ class Voice(object):
 		while(1):
 			try:
 				self.inputBuffer = KeypadLogic.getInput(self.speaker)
-				print "Input Buffer: " + self.inputBuffer
-				ns.sem=1
 				strInput = ''.join(self.inputBuffer)
+				
 				print strInput
-				ns.sem=0
 
 				if (strInput == '*1'):
 
